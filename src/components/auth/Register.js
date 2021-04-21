56abec82a17d50426f4b55c27ac5743ff222c0c2
xatreyux <mwmailing@gmail.com>
2021-04-21 16:10:55 -0400

    added register axios

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, { useState } from "react";
 
 const Register = () => {
@@ -5,13 +6,25 @@ const Register = () => {
   const [password, setPassword] = useState("");
   const [passwordVerify, setPasswordVerify] = useState("");
 
+  const register = async (e) => {
+    e.preventDefault();
+    try {
+      const register = {
+        email, password, passwordVerify
+      }
+      await axios.post("http://localhost:5000/auth/", registerData)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
   return (
     <div>
       <h1>Register a new account</h1>
-      <form>
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
-        <input type="passwordVerify" placeholder="Verify your password" />
+      <form onSubmit={register}>
+        <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} value={email} />
+        <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} valie={password} />
+        <input type="passwordVerify" placeholder="Verify your password" onChange={(e) => setPasswordVerify(e.target.value)} value={passwordVerify} />
         <button type="submit">Register</button>
       </form>
     </div>
